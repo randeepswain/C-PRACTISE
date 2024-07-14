d3adf81528c28d477096154faf67fd996decd230
Randeep Swain <randeeptech42@gmail.com>
2024-07-14 08:15:46 +0530

    chapter 4

--- CHAP4_LOOP/C4PSET/C4PSET1.c
+++ CHAP4_LOOP/C4PSET/C4PSET1.c
@@ -1,34 +1,39 @@
 // Write a program to check whether a given number is prime or not using loops.
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int num,i,isprime=0;
+int main()
+{
+    int num, i, isprime = 0;
 
-printf("Enter the number: ");
-scanf("%d",&num);
+    printf("Enter the number: ");
+    scanf("%d", &num);
 
-if (num<2)
-{                               
-    isprime = 0;
-}
+    if (num < 2)
+    {
+        isprime = 0;
+    }
 
+    for (i = 2; i < num; i++)
+    {
 
-for(i=2;i<num;i++){
-
- if(num%i == 0){
-    isprime = 0;
-    break;
- }
- else{
-    isprime = 1;
- }
-}
-if((isprime==0) && (num!=2)){
-   printf("%d is not a prime number",num);
-}
-else{
-   printf("%d is a prime number",num);
-}
+        if (num % i == 0)
+        {
+            isprime = 0;
+            break;
+        }
+        else
+        {
+            isprime = 1;
+        }
+    }
+    if ((isprime == 0) && (num != 2))
+    {
+        printf("%d is not a prime number", num);
+    }
+    else
+    {
+        printf("%d is a prime number", num);
+    }
     return 0;
 }
--- CHAP4_LOOP/C4T1.c
+++ CHAP4_LOOP/C4T1.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int i=0,j=0,k;
-
+int main()
+{
+    int i = 0, j = 0, k;
 
     // while loop
 
-    while(i<9){
-        printf("VALUE OF i is %d\n",i);
+    while (i < 9)
+    {
+        printf("VALUE OF i is %d\n", i);
         i = i + 1;
     }
-    
 
-    //Infinite while loop
+    // Infinite while loop
 
     // while(i>=0){
     //     printf("VALUE OF i is %d\n",i);
@@ -21,28 +21,30 @@ int main(){
 
     // Write a program to print natural numbers from 10 to 20 when initial loop counter is initialized to 0.
     printf("Natural number from 10 to 20\n");
-    while(j>=0){
-        
-        if (j>=10 && j<=20){
-            
-            printf("%d\n",j);
+    while (j >= 0)
+    {
+
+        if (j >= 10 && j <= 20)
+        {
+
+            printf("%d\n", j);
         }
         j++;
     }
-
 
     // do while loop   --executes the loop atleast once
     i = 0;
     do
     {
-      printf("value of i is %d\n",i);
-      i++; 
+        printf("value of i is %d\n", i);
+        i++;
     } while (i < 4);
 
-    // for loop 
-    for (i = 0; i < 5;i++ ){
-        printf("value of i is %d\n",i);
+    // for loop
+    for (i = 0; i < 5; i++)
+    {
+        printf("value of i is %d\n", i);
     }
-    
-   return 0; 
+
+    return 0;
 }
